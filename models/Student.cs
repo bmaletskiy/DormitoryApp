5b9be3aeec106654c6f13e7a1696bcafde0c7219
bmaletskiy <bmaletskij@knu.ua>
2025-11-30 00:40:22 +0200

    the last commit

--- models/Student.cs
+++ models/Student.cs
@@ -2,14 +2,14 @@ namespace DormitoryApp.Models
 {
     public class StudentInfo
     {
-        public string Name { get; set; }
-        public string Faculty { get; set; }
-        public string Department { get; set; }
-        public string Room { get; set; }
-        public string Bed { get; set; }
-        public string Course { get; set; }
-        public string StartDate { get; set; }
-        public string EndDate { get; set; }
+        public string? Name { get; set; }
+        public string? Faculty { get; set; }
+        public string? Department { get; set; }
+        public string? Room { get; set; }
+        public string? Bed { get; set; }
+        public string? Course { get; set; }
+        public string? StartDate { get; set; }
+        public string? EndDate { get; set; }
 
         public override string ToString()
         {
